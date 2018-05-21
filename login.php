a6d47d04b47396921cdfbe5700bd8d54010588a8
salvo1994tpe <salvo1994tpe@gmail.com>
2018-05-21 15:12:16 +0200

    versione lancio 1.1

--- login.php
+++ login.php
@@ -8,13 +8,25 @@ include "db_connessione.php";
 							if($count > 0){
 							     
 								  $_SESSION['logged'] = true;
+								  Header("Location: home.php");
 								}
 							else{
 					
 								$_SESSION['logged'] = false;
+								include("inclusioni.php");
+								echo("<div class='container'>");
+									echo("<div class='row text-center'>");
+										echo("<div class='col'>");
+											echo("<p> Username o password errati </p> </br>");
+											echo("<a href='index.php'> Ritorna alla schermata login  </a>");
+										echo("</div>");
+									echo("</div>");
+								echo("</div>");
 							}
 							
-						echo($_SESSION['logged']);
+
 						
 
-				Header("Location: home.php");
+			?>
+			
+			
